fb0cc8b4b36006a47fe083ca11d16780afe24921
SACHINQEEP <patelsachinsp269@gmail.com>
2022-06-26 19:06:33 +0530

    created Loan Table for loan process

--- src/entity/Loan.ts
+++ src/entity/Loan.ts
@@ -1,22 +1,87 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity()
-export class Loan extends BaseEntity{
-    @PrimaryGeneratedColumn("uuid")
-    loan_id: string
+export class Loan extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  loan_id: string
 
-    @Column()
-    account_number: string
+  @Column()
+  first_name: string
 
-    @Column()
-    interest_rate: number
+  @Column()
+  last_name: string
 
-    @Column()
-    duration: string
+  @Column({
+    type: 'simple-json',
+    nullable: true
+  })
+  family_details: {
+    father_name: string
+    mother_name: string
+    occupation: string
+  }
 
-    @Column()
-    start_date: Date
+  @Column()
+  branch_name: string
 
-    @Column()
-    due_amount: number
+  @Column({
+    unique: true
+  })
+  email_id: string
+
+  @Column()
+  account_number: string
+
+  @Column()
+  mobile_number: string
+
+  @Column()
+  aadhar_card_number: string
+
+  @Column()
+  pan_card_number: string
+
+  @Column({
+    type: 'simple-json',
+    nullable: true
+  })
+  docs: {
+    aadhar_card: string
+    pan_card: string
+    address_proof: string
+    transection_history: string
+  }
+
+  @Column({
+    type: 'simple-json',
+    nullable: true
+  })
+  aadress: {
+    line: string
+    near_by: string
+    pin_code: number
+    city: string
+    state: string
+  }
+
+  @Column()
+  amount_requested: number
+
+  @Column()
+  interest_rate: number
+
+  @Column()
+  loan_duration: string
+
+  @Column()
+  start_date: Date
+
+  @Column()
+  end_date: Date
+
+  @Column()
+  time_period: Date
+
+  @Column()
+  due_amount: number
 }
